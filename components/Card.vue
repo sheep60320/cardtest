a17d4772d7a02884bf59cb8bdc5ef3f00223de7a
Ian_Yang_20159 <82049134+sheep60320@users.noreply.github.com>
2022-07-11 17:25:56 +0800

    Initial commit

---
+++ components/Card.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="box-row">
+    <div class="box-col" v-for="(item, key) in cards" :key="key">
+      <div class="img">
+        <img :src="item.previewPictureSrc" alt="" />
+      </div>
+      <div class="title">
+        {{ item.title }}
+      </div>
+      <div class="text">
+        {{ item.previewContext }}
+      </div>
+      <button>立即前往></button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["cards"],
+  created() {
+    console.log(this.cards);
+  },
+};
+</script>
+
+<style scoped >
+.box-row {
+  box-sizing: border-box;
+  margin: 20px auto;
+  width: 1029px;
+  height: 388px;
+  display: flex;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  overflow: hidden;
+
+  /* background-color: aqua; */
+}
+.box-row .box-col {
+  margin-right: 30px;
+  width: 323px;
+  height: 100%;
+}
+.box-row .box-col:nth-child(3) {
+  margin-right: 0;
+}
+.box-row .img {
+  width: 100%;
+  height: 170px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+button {
+  background-color: gray;
+  color: #fff;
+  padding: 10px;
+  margin: 18px auto;
+  width: 108px;
+  height: 40px;
+  font-size: 14px;
+  display: block;
+}
+.box-row .title {
+  box-sizing: border-box;
+  width: 100%;
+  font-size: 20px;
+  text-align: center;
+  padding: 15px;
+}
+.box-row .text {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 15px;
+  font-size: 16px;
+  height: 100px;
+  overflow: hidden;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div>
+      <Card :cards="cards" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "~/components/Card.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      cards: [],
+      apiUrl: "https://event.smarter.com.tw/api/spa/bestseller/interview",
+    };
+  },
+  created() {
+    axios
+      .get(this.apiUrl)
+      .then((response) => {
+        //console.log(response);
+        this.cards = response["data"]["data"];
+        console.log(this.cards);
+      })
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style scoped >
+.container {
+  box-sizing: border-box;
+  max-width: 1029px;
+}
+</style>
